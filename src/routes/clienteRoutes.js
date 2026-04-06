752d40e9cc5241df6491686e52d629f97f264d20
LUÍSA GUEDES PEREIRA BAZAN <luisabazan36@gmail.com>
2026-04-06 13:06:14 -0300

    s1-r3

--- src/routes/clienteRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import clienteController from "../controllers/clienteController.js";
-
-const clienteRoutes = Router();
-
-clienteRoutes.post('/', clienteController.criar);
-clienteRoutes.get('/', clienteController.selecionar);
-
-export default clienteRoutes;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,8 @@ const routes = Router();
 
 import categoriaRoutes from "./categoriaRoutes.js";
 import produtoRoutes from "./produtoRoutes.js";
-import clienteRoutes from "./clienteRoutes.js"; 
 
 routes.use('/categorias', categoriaRoutes);
 routes.use('/produtos', produtoRoutes);
-routes.use('/clientes', clienteRoutes); 
 
 export default routes;
